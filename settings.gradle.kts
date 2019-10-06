17a57d61d1ed405f6756f41b1c4b42828af68271
Jean-Michel Fayard <jean-michel.fayard@akelius.de>
2019-10-06 23:03:14 +0200

    Upgrade plugins - painful
    https://kotlinlang.org/docs/tutorials/android-plugin.html

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,16 @@
+pluginManagement {
+    repositories {
+    }
+
+    val resolutionStrategyConfig: String? by extra
+    resolutionStrategy.eachPlugin {
+        val property = "plugin.${requested.id.id}"
+        if (extra.has(property) && resolutionStrategyConfig != "false") {
+            val version = extra.get(property) as String
+            if (resolutionStrategyConfig == "verbose") println("ResolutionStrategy selected version=$version from property=$property")
+            useVersion(version)
+        }
+    }
+}
 rootProject.name = "Update legacy rootProject"
 include(":app")
