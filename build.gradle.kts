f60799a09c23fe7e320d4e34b156a10f6b4a0203
Jean-Michel Fayard <jmfayard@gmail.com>
2019-10-26 21:00:44 +0200

    gradle: super nice tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
 }
 
 refreshVersions {
-    useFqdnFor("espresso-core", "core-testing", "appcompat", "material", "aapt2", "constraintlayout", "lint-gradle")
+    useFqdnFor("espresso-core", "core-testing", "appcompat", "material", "aapt2", "constraintlayout", "lint-gradle", "annotations")
     orderBy = OrderBy.GROUP_AND_ALPHABETICAL
 }
 
@@ -27,19 +27,40 @@ buildScan {
     publishAlways()
 }
 
-tasks.create("tests") {
+tasks.register("tests") {
     group = "custom"
     description = "Run the unit tests"
+    dependsOn(":app:testDebugUnitTest")
 }
 
-tasks.create("install") {
+tasks.register("install") {
     group = "custom"
     description = "Install the app"
     dependsOn(":app:installDebug")
 }
 
-tasks.create("hello") {
+tasks.register("hello") {
     group = "custom"
     description = "Empty Hello World task, useful to debug build problems"
 }
 
+tasks.register<Delete>("clean") {
+    group = "custom"
+    description = "Delete build files"
+    delete = setOf(".gradle", ".idea", "**.iml", "build", "app/build")
+}
+
+tasks.register<Copy>("apk") {
+    group = "custom"
+    description = "Build a release APK"
+    val apkName = "${project.name}-versionCode-${findProperty("versionCode")}.apk"
+    dependsOn(":app:assembleRelease")
+    from("app/build/outputs/apk/release") {
+        include("app-released-unsigned.apk")
+        rename(".*", apkName)
+    }
+    into("build/apks")
+    doLast {
+        println("Created apk: " + project.file("build/apks/$apkName").absolutePath)
+    }
+}
