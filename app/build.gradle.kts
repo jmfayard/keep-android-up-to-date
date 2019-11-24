4ac6ddf1beada5a0b603998e9031cb2602ecd621
Louis CAD <louis.cognault@gmail.com>
2019-11-24 12:49:19 +0100

    Fix dependencies constants

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,19 +24,19 @@ dependencies {
   implementation(AndroidX.lifecycle.runtime)
   implementation(AndroidX.lifecycle.extensions)
   kapt(AndroidX.lifecycle.compiler)
-  //implementation(Kotlin.stdlibJdk7)
+  implementation(Kotlin.stdlib.jdk7)
 
   testImplementation(Testing.junit4)
 
-  //testImplementation(Testing.mockitoCore)
-  //testImplementation(Testing.mockitoKotlin)
-  //testImplementation(Testing.mockitoInline)
+  testImplementation(Testing.mockito.core)
+  testImplementation(Testing.mockito.kotlin)
+  testImplementation(Testing.mockito.inline)
   testImplementation(AndroidX.archCore.testing)
 
 
-  //androidTestImplementation(Testing.mockitoCore)
-  //androidTestImplementation(Testing.mockitoAndroid)
-  //androidTestImplementation(Testing.mockitoKotlin)
+  androidTestImplementation(Testing.mockito.core)
+  androidTestImplementation(Testing.mockito.android)
+  androidTestImplementation(Testing.mockito.kotlin)
 
   androidTestImplementation(AndroidX.test.core)
   androidTestImplementation(AndroidX.archCore.testing)
